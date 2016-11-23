021a95f741f03df6f3e8713a33694addd04b9377
sunnyleeyun <sunnylee945@gmail.com>
2016-11-23 11:49:24 +0800

    tests

--- Grocery List/GroceryListTableViewController.swift
+++ Grocery List/GroceryListTableViewController.swift
@@ -4,20 +4,38 @@
 //
 //  Created by Sunny on 2016/10/12.
 //  Copyright © 2016年 Sunny. All rights reserved.
-//
+
 
 import UIKit
+import CoreData
 
 class GroceryListTableViewController: UITableViewController {
 
+    var groceries = [Grocery]()
+    var managedObjectContext: NSManagedObjectContext?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        managedObjectContext = appDelegate.persistentContainer.viewContext
+    
+        loadData()
+    }
+    
+    func loadData(){
+        //let request: NSFetchRequest<NSManagedObject> = NSFetchRequest(entityName: "Grocery")
+        
+        let request: NSFetchRequest<Grocery> = Grocery.fetchRequest()
+        do{
+            let results = try managedObjectContext?.fetch(request)
+            groceries = results!
+            tableView.reloadData()
+        }
+        catch{
+            fatalError("Error in retrieving Grocery item")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,27 +43,68 @@ class GroceryListTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addAction(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Grocery Item", message: "What's to buy now", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addTextField { (textField: UITextField) in
+            
+        }
+        let addAction = UIAlertAction(title: "ADD", style: UIAlertActionStyle.default) { [weak self](action: UIAlertAction) in
+            /*let textField = alertController.textFields?.first
+            //self?.groceries.append(textField!.text!)
+            
+            let entity = NSEntityDescription.entity(forEntityName: "Grocery", in: (self?.managedObjectContext)!)
+            let grocery = NSManagedObject(entity: entity!, insertInto: self?.managedObjectContext)
+            grocery.setValue(textField?.text!, forKey: "item")*/
+            
+            let itemString: String?
+            
+            if(alertController.textFields?.first?.text != ""){
+                itemString = alertController.textFields?.first?.text
+            }else{
+                return
+            }
+            
+            let grocery = Grocery(context: (self?.managedObjectContext)!)
+            grocery.item = itemString
+            
+            do{
+                try self?.managedObjectContext?.save()
+            }
+            catch{
+                fatalError("Error in storing data")
+            }
+            
+            
+            
+            self?.loadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil)
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return groceries.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "groceryCell", for: indexPath)
+        let grocery = self.groceries[indexPath.row]
+        //cell.textLabel?.text = grocery.value(forKey: "item") as? String
+        cell.textLabel?.text = grocery.item
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
